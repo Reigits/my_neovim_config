b36aea9236032fff958926b432600b41ed408c8e
Reigits <rafarobyan1@gmail.com>
2026-07-06 19:40:13 +0700

    changes something in lualine.lua

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,33 +1,33 @@
 return
 {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    opts = 
+'nvim-lualine/lualine.nvim',
+dependencies = { 'nvim-tree/nvim-web-devicons' },
+opts = 
+{
+    options = 
     {
-	options = 
-	{
-		theme = '16color'
-	},
-	sections =
-	{
-		lualine_b = 
-		{
-			{
-				'branch'
-			},
-			{
-				'diff',
-				diff_color = 
-				{
-					added    = { fg = '#39ff14' },
-					modified = { fg = '#ff8700' },
-					removed  = { fg = '#ff4500' },
-				}
-			},
-			{
-				'diagnostics'
-			},	
-		}
-	}
+        theme = '16color'
+    },
+sections =
+{
+    lualine_b = 
+    {
+        {
+            'branch'
+        },
+        {
+            'diff',
+            diff_color = 
+            {
+                added    = { fg = '#39ff14' },
+                modified = { fg = '#ff8700' },
+                removed  = { fg = '#ff4500' },
+            }
+        },
+        {
+            'diagnostics'
+        },	
     }
+}
+}
 }
